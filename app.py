918d5d73d008771e2684949d57cc14e2b30e8729
banorani <bano1@users.noreply.huggingface.co>
2025-08-30 07:00:35 +0000

    Update app.py

--- app.py
+++ app.py
@@ -176,8 +176,8 @@ def export_bibtex(papers):
     return bibtexparser.dumps(db)
 
 # ================== TABS ==================
-tabs = st.tabs(["ℹ️ About", "📑 Paper Summaries", "📊 Topic Modeling",
-                "📂 Collections", "⚡ Trends & Insights", "📤 Upload Papers"])
+tabs = st.tabs(["ℹ️ About","📤 Upload Papers" "📑 Paper Summaries", "📊 Topic Modeling",
+                "📂 Collections", "⚡ Trends & Insights"])
 
 # --- About ---
 with tabs[0]:
