d5f0caef5e861cdc0ff485b73b90ad85d0199d21
banorani <bano1@users.noreply.huggingface.co>
2026-07-29 15:06:36 +0000

    Create chat.py

---
+++ src/chat.py
@@ -0,0 +1,44 @@
+import streamlit as st
+from src.ai_functions import client
+
+
+def chat_section():
+
+    st.header("💬 Chat With Papers")
+
+    context = "\n".join(
+        [
+            p["abstract"]
+            for p in st.session_state.papers
+        ]
+    )
+
+
+    question = st.text_area(
+        "Ask your question"
+    )
+
+
+    if st.button("Ask"):
+
+        response = client.chat.completions.create(
+            model="llama-3.1-8b-instant",
+            messages=[
+                {
+                    "role":"user",
+                    "content":
+                    f"""
+                    Context:
+                    {context}
+
+                    Question:
+                    {question}
+                    """
+                }
+            ]
+        )
+
+
+        st.write(
+            response.choices[0].message.content
+        )
